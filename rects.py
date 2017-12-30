da8b89cd8198a4015d2aa19ab323dd03cfd0c05a
prakharchoudhary <prakhar2397@gmail.com>
2017-12-31 00:39:34 +0530

    add depth camera usage

--- rects.py
+++ rects.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import utils
 
 
 def outlineRect(image, rect, color):
@@ -9,7 +11,7 @@ def outlineRect(image, rect, color):
     cv2.rectangle(image, (X, y), (X + w, y + h), color)
 
 
-def copyRect(src, dst, srcRect, dstRect,
+def copyRect(src, dst, srcRect, dstRect, mask=None,
              interpolation=cv2.INTER_LINEAR):
     """Copy part of the source to part of the destination"""
     x0, y0, w0, h0 = [int(i) for i in srcRect]
@@ -17,11 +19,23 @@ def copyRect(src, dst, srcRect, dstRect,
 
     # Resize the contents of the source sub-rectangle
     # Put the result in the destination subrectangle
-    dst[y1:y1 + h1, x1:x1 + w1] = cv2.resize(src[y0:y0 + h0, x0:x0 + w0], (w1, h1),
-                                             interpolation=interpolation)
+    if mask is None:
+        dst[y1:y1 + h1, x1:x1 + w1] = cv2.resize(src[y0:y0 + h0, x0:x0 + w0], (w1, h1),
+                                                 interpolation=interpolation)
+    else:
+        if not utils.isGray(src):
+            # Convert the mask to 3 channels, like the image.
+            mask = mask.repeat(3).reshape(h0, w0, 3)
+        # Perform the copy, with the mask applied.
+        dst[y1:y1 + h1, x1:x1 + w1] = np.where(cv2.resize(mask,
+                                                          (w1, h1),
+                                                          interpolation=cv2.INTER_LINEAR),
+                                               cv2.resize(src[y0:y0 + h0, x0:x0 + w0], (w1, h1),
+                                                          interpolation=interpolation),
+                                               dst[y1:y1 + h1, x1:x1 + w1])
 
 
-def swapRects(src, dst, rects,
+def swapRects(src, dst, rects, masks=None,
               interpolation=cv2.INTER_LINEAR):
     """Copy the source with two or more sub-rectangles swapped."""
     if dst is not src:
@@ -31,6 +45,9 @@ def swapRects(src, dst, rects,
     if numRects < 2:
         return
 
+    if masks is None:
+        masks = [None] * numRects
+
     # Copy the contents of last rectangle into temporary storage.
     x, y, w, h = rects[numRects - 1]
     temp = src[y:y + h, x:x + w].copy()
@@ -38,8 +55,10 @@ def swapRects(src, dst, rects,
     # Copy the contents of each rectangle into next
     i = numRects - 2
     while i >= 0:
-        copyRect(src, dst, rects[i], rects[i + 1], interpolation)
+        copyRect(src, dst, rects[i], rects[i + 1], masks[i],
+                 interpolation)
         i -= 1
 
     # Copy the temporarily stored content into the first rectangle
-    copyRect(temp, dst, (0, 0, w, h), rects[0], interpolation)
+    copyRect(temp, dst, (0, 0, w, h), rects[0], masks[numRects - 1],
+             interpolation)
